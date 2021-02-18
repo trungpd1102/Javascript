c1851135d291142964df0ce95861b902e4d5116c
trung1102 <letsmile95@gmail.com>
2021-02-18 23:08:45 +0700

    update

---
+++ coderx/data_type/object.js
@@ -0,0 +1,13 @@
+// var a = {
+//     key: value
+// };
+
+var myCat = {
+    name: 'meo',
+    weight: 2.5,
+    isAlive: true
+};
+
+myCat.name = 'dog';
+console.log(myCat.name);
+console.log(myCat['name'])
