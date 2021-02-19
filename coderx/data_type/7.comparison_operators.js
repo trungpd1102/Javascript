bd03935baee5e2b5ab5004cd2c37632b9d978039
trung1102 <letsmile95@gmail.com>
2021-02-19 23:43:03 +0700

    update

---
+++ coderx/data_type/7.comparison_operators.js
@@ -0,0 +1,27 @@
+//  Các phép toán so sánh
+
+// var a = 10 !== 10;
+// var b = 10 === 10;
+
+// console.log(a);
+// console.log(b);
+
+
+// var a = {
+//     name: 'a'
+// };
+// var b = {
+//     name: 'a'
+// };
+
+// var c = a === b; //false
+
+// console.log(c);
+
+
+var a =[1,2,3];
+var b =[1,2,3];
+
+var c = a === b; //false
+
+console.log(c);
---
+++ coderx/data_type/8.for_loop.js
@@ -0,0 +1,13 @@
+// for( i = 0; i<5; i++ ) {
+//     console.log(i);
+// };
+
+var employees = [
+    {name: 'Trung', age: 26},
+    {name: 'Ngoc', age: 28},
+    {name: 'Nguyet', age: 23},
+];
+
+for (var i = 0; i < employees.length; i++){
+    console.log(employees[i].name, employees[i].age);
+};
