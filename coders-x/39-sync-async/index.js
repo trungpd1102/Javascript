acb908d9849e05d91d429d927a4762df0c2d1734
trung1102 <letsmile95@gmail.com>
2021-02-23 16:01:20 +0700

    update

---
+++ coders-x/39-sync-async/index.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+// Synchronous
+console.log("Start");
+var song1 = fs.readFileSync('./song1.txt', encoding = 'utf8');
+console.log(song1);
+var song2 = fs.readFileSync('./song2.txt', encoding = 'utf8');
+console.log(song2);
+console.log("End");
+
+// Asynchoronous
+console.log('Start');
+fs.readFile('./song1.txt', encoding = 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+}); 
+console.log('End');
