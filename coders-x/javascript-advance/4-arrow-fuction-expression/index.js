d86a92bda12a847890ff83ddb3a3e519b78b4c81
ductrung1102 <letsmile95@gmail.com>
2021-04-15 23:57:56 +0700

    update 15/4

---
+++ coders-x/javascript-advance/4-arrow-fuction-expression/index.js
@@ -0,0 +1,29 @@
+// arrow function expression
+// fat arrow
+
+// function sum(a, b) {
+//     return a+b
+// }
+// console.log(sum(4, 5));
+
+// var sum2 = function (a, b) {
+//     return a+b
+// }
+// console.log(sum2(2, 3));
+
+// var sum3 = (a, b) => {
+//     return a+b
+// }
+// console.log( sum3(3,9));
+
+// var square = (a) => a*a
+// console.log(square(2));
+
+// var square1 = b => b*2
+// console.log(square1(3));
+
+var arr = [1, 2, 4]
+
+var value = arr.map( (index) => index*2)
+console.log(value);
+console.log(arr);
